e8cd175aae25382a5fc15470246fecc94ab10bf7
206624202 <k.schmidt2@gmail.com>
2021-03-23 13:02:19 -0400

    adds chart

--- app/js/main.js
+++ app/js/main.js
@@ -6,9 +6,9 @@ Highcharts.setOptions({
 
 
 function drawHighcharts() {
-    Highcharts.chart("chart-container-PROJECT-NAME", { // add the proper ID here
+    Highcharts.chart("chart-container-bull-one-year", { // add the proper ID here
         chart: {
-            type: 'bar',
+            type: 'line',
             styledMode: true,
             spacingBottom: 25,
             spacingRight: 100,
@@ -19,31 +19,25 @@ function drawHighcharts() {
             text: null
         },
         data: {
-            googleSpreadsheetKey: '1YOKb5l2VM4aAB2r20N_1aT_1vEajYrP3U-U3A6lZbC0',
-        },
-        // for bar charts only
-        plotOptions: {
-            series: {
-                groupPadding: 0.1
-            } 
+            googleSpreadsheetKey: '17Ql0YYsBM29J-qIuWYxL0o2NP67QLj8VBkvt8WD_Abk',
         },
         // for line charts only
-        // plotOptions: {
-        //     series: {
-        //         lineWidth: 1,
-        //         // clip: false,
-        //         marker: {
-        //             enabled: false,
-        //             symbol: 'circle',
-        //             fillColor: '#ffffff',
-        //             states: {
-        //                 hover: {
-        //                     fillColor: '#ffffff'
-        //                 }
-        //             }
-        //         }
-        //     }
-        // },
+        plotOptions: {
+            series: {
+                lineWidth: 1,
+                // clip: false,
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    fillColor: '#ffffff',
+                    states: {
+                        hover: {
+                            fillColor: '#ffffff'
+                        }
+                    }
+                }
+            }
+        },
         legend: {
             align: 'right',
             symbolRadius: 0,
@@ -85,9 +79,10 @@ function drawHighcharts() {
         tooltip: {
             shadow: false,
             padding: 10,
-            // valueSuffix: '',
+            shared: true,
+            valueSuffix: '%',
             // valuePrefix: '',
-            // valueDecimals: 2,
+            valueDecimals: 2,
         },
         responsive: {
             rules: [{
@@ -97,6 +92,117 @@ function drawHighcharts() {
             chartOptions: {
                 chart: {
                 spacingRight: 10
+                },
+                legend: {
+                    align: 'left',
+                    x: -8
+                },
+                tooltip: {
+                    enabled: false
+                }
+            }
+            }]
+        }
+    }),
+    Highcharts.chart("chart-container-bull-one-year-2", { // add the proper ID here
+        chart: {
+            type: 'bar',
+            styledMode: true,
+            spacingBottom: 25,
+            spacingRight: 100,
+            spacingLeft: 0,
+            spacingTop: 25
+        }, 
+        title: {
+            text: null
+        },
+        data: {
+            googleSpreadsheetKey: '17Ql0YYsBM29J-qIuWYxL0o2NP67QLj8VBkvt8WD_Abk',
+            googleSpreadsheetWorksheet: 2
+        },
+        // for bar charts only
+        plotOptions: {
+            series: {
+                groupPadding: 0.1,
+                clip: false,
+            } 
+        },
+        // for line charts only
+        // plotOptions: {
+        //     series: {
+        //         lineWidth: 1,
+        //         // clip: false,
+        //         marker: {
+        //             enabled: false,
+        //             symbol: 'circle',
+        //             fillColor: '#ffffff',
+        //             states: {
+        //                 hover: {
+        //                     fillColor: '#ffffff'
+        //                 }
+        //             }
+        //         }
+        //     }
+        // },
+        legend: {
+            enabled: false
+        },
+        xAxis: {
+            labels: {
+                style: {
+                    whiteSpace: 'nowrap'
+                },
+                // edits xAxis ticks
+                // dateTimeLabelFormats: {
+                //     week: '%b. %e',
+                // },
+            },
+            tickLength: 5,
+            // tickInterval: 24 * 3600 * 1000 * 7
+        },
+        yAxis: {
+            title: false,
+            labels: {
+                useHTML: true,
+                overflow: 'allow',
+                            formatter: function () {
+                return Highcharts.numberFormat(this.value,0,'.',',');
+            },
+            },
+            plotLines: [{
+                width: 2,
+                value: 76.12,
+                zIndex: 5,
+                label: {
+                    text: 'S&P 500',
+                    align: 'center',
+                    y: -5,
+                    rotation: 0
+                }
+            }],
+            // min: ,
+            max: 1000,
+            // tickAmount: ,
+            // adds commas to thousands
+        },
+        credits: {
+            enabled: false
+        },
+        tooltip: {
+            shadow: false,
+            padding: 10,
+            valueSuffix: '%',
+            // valuePrefix: '',
+            valueDecimals: 2,
+        },
+        responsive: {
+            rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                chart: {
+                spacingRight: 20
                 },
                 legend: {
                     align: 'left',
